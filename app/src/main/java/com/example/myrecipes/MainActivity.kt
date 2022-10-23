547ece86ce47cdd928ff011a15c5862ffc0604c3
Arisemcdc <arisemcdc1@gmail.com>
2022-10-23 11:53:10 +0300

    add timber dependancy

--- app/src/main/java/com/example/myrecipes/MainActivity.kt
+++ app/src/main/java/com/example/myrecipes/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.myrecipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.d
+import timber.log.Timber
+import timber.log.Timber.Forest.d
+import timber.log.Timber.Forest.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /*timber.log.d("MainActivity","This is log for OnCreate")*/
+        /*log(1, "MainActivity", "This is log for OnCreate")*/
+        Log.d("MainActivity", "This is for OnCreate")
+        timber.log.Timber.d("MainActivity","This is for onCreate111")
+        Timber.d("Hello","1")
+
     }
 }
